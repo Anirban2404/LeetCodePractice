5a702c6f7f2da46d1cbb3eed5a50a2e76a1d4f63
Anirban Bhattacharjee <anirban-mac@Anirbans-MacBook-Pro.local>
2019-06-08 10:37:22 -0500

    commit

---
+++ 146_LRUCache.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  7 20:58:20 2019
+
+@author: anirban-mac
+"""
+
+"""
+146. LRU Cache
+Design and implement a data structure for Least Recently Used (LRU) cache. 
+It should support the following operations: get and put.
+
+get(key) - Get the value (will always be positive) of the key if the key 
+exists in the cache, otherwise return -1.
+put(key, value) - Set or insert the value if the key is not already present. 
+When the cache reached its capacity, it should invalidate the least recently 
+used item before inserting a new item.
+
+The cache is initialized with a positive capacity.
+
+Follow up:
+Could you do both operations in O(1) time complexity?
+
+Example:
+
+LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+"""
+"""
+from collections import OrderedDict
+class LRUCache(OrderedDict):
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if key not in self:
+            return -1
+        self.move_to_end(key)
+        return self[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self:
+            self.move_to_end(key)
+        self[key] = value
+        if len(self) > self.capacity:
+            self.popitem(last = False)
+
+"""
+class Node:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.dic = dict()
+        self.prev = self.next = self
+        
+    def get(self, key):
+        if key in self.dic:
+            n = self.dic[key]
+            self._remove(n)
+            self._add(n)
+            return n.val
+        return -1
+
+    def put(self, key, value):
+        if key in self.dic:
+            self._remove(self.dic[key])
+        n = Node(key, value)
+        self._add(n)
+        self.dic[key] = n
+        if len(self.dic) > self.capacity:
+            n = self.next
+            self._remove(n)
+            del self.dic[n.key]
+
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+    def _add(self, node):
+        p = self.prev
+        p.next = node
+        self.prev = node
+        node.prev = p
+        node.next = self
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
--- 939_minAreaRect.py
+++ 939_minAreaRect.py
@@ -32,3 +32,36 @@ Note:
 0 <= points[i][1] <= 40000
 All points are distinct.
 """
+from collections import defaultdict
+
+class Solution:
+    def minAreaRect(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        cols = defaultdict(list)
+        for x, y in points:
+            cols[x].append(y)
+        print (cols)
+        minArea = float('inf')
+        prevX = {}
+        
+        for x in sorted(cols):
+            col = cols[x]
+            col.sort()
+            #print(x, col)
+            for j, y2 in enumerate(col):
+                for i in range(j):
+                    y1 = col[i]
+                    print(y1,y2)
+                    if (y1, y2) in prevX:
+                        print("-->", prevX[y1, y2], x)
+                        minArea = min(minArea, (x - prevX[y1, y2]) * (y2 -y1))
+                    prevX[y1,y2] = x
+                    
+        return minArea if minArea < float('inf') else 0
+                    
+                    
+points = [[1,1],[1,3],[3,1],[3,3],[4,1],[4,3]]
+print(Solution().minAreaRect(points))
