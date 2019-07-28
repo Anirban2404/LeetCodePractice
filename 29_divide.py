0fd9903a3e218de9f88886da63f268cdb6f26bc7
Anirban Bhattacharjee <anirban-mac@Anirbans-MacBook-Pro.local>
2019-07-28 14:55:44 -0500

    commit

---
+++ 29_divide.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 28 12:53:24 2019
+
+@author: anirban-mac
+"""
+
+"""
+Given two integers dividend and divisor, divide two integers without using 
+multiplication, division and mod operator.
+
+Return the quotient after dividing dividend by divisor.
+
+The integer division should truncate toward zero.
+
+Example 1:
+
+Input: dividend = 10, divisor = 3
+Output: 3
+Example 2:
+
+Input: dividend = 7, divisor = -3
+Output: -2
+"""
+
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        if divisor == 0:
+            return 
+        if (dividend == -2**31 + 1 and divisor == -1): 
+            return -2**31 + 1
+        _dividend = (dividend)
+        _divisor = abs(divisor)
+        res = 0
+
+        positive = (dividend < 0) is (divisor < 0)
+        dividend, divisor = abs(dividend), abs(divisor)
+        res = 0
+        while dividend >= divisor:
+            temp, i = divisor, 1
+            while dividend >= temp:
+                dividend -= temp
+                res += i
+                i <<= 1
+                temp <<= 1
+        if not positive:
+            res = -res
+        return res
+            
+
+dividend = -10
+divisor = 2
+print(Solution().divide(dividend,divisor))
